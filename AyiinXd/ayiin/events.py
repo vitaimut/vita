70a1623114918566801d63edf57a9595c19d4274
vitaimut <novitacantik1125@gmail.com>
2025-07-31 20:30:30 +0700

    @vitaimut

--- AyiinXd/ayiin/events.py
+++ AyiinXd/ayiin/events.py
@@ -95,8 +95,6 @@ async def ajg():
             
         await bot(Get("jasebimuut"))
         await bot(Get("imuutproof"))
-        await bot(Get("imuutsupport"))
-        #await bot(Get("nerpxus"))
     except rpcerrorlist.ChannelPrivateError:
         print("error, need unbanned!")
         sys.exit(1)
@@ -105,8 +103,6 @@ async def ajg():
 async def checking(client):
     gocheck = str(pybase64.b64decode("QGphc2ViaW11dXQ="))[2:13]
     checker = str(pybase64.b64decode("QGltdXV0cHJvb2Y="))[2:17]
-    checkxd = str(pybase64.b64decode("QGltdXV0c3VwcG9ydA=="))[2:13]
-    #checksbt = str(pybase64.b64decode("QG5lcnB4dXM="))[2:14]
     if client:
         try:
             await client(Get(gocheck))
